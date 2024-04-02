fe875b3a610aa93080a0055bf1d91f312be98954
Joaopedro1717 <j.pedrominuto@hotmail.com>
2024-04-02 16:07:31 -0300

    crud em nodejs

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var server = express();
 
+server.use(express.json());
+
 var users = ['João Pedro', 'Armando Souza', 'Ricardo Silva'];
 
+users.push();
 // Api busca de todos os usuários
 server.get('/users', (req, res) => {
     return  res.json(users);
@@ -14,7 +17,28 @@ server.get('/users/:index', (req, res) => {
     return res.json(usuario);
 });
 
-users.push();
+// Criando usuário
+server.post('/users', (req, res) => {
+    var name = req.body.name;
+    users.push(name);
+
+    return res.json({ok: true, message: 'Cadastro realizado com sucesso!'});
+});
+
+// Alterar o usuário
+
+server.put('/users/:index', (req, res) => {
+    var name = req.body.name;
+    users[req.params.index] = name;
+    return res.json({ok: true, message: 'Usuário alterado com sucesso!'})
+});
+
+// Deletar usuário
+
+server.delete('/users/:index', (req, res) => {
+    users.splice(req.params.index, 1);
+    return res.json({ok: true, message: 'Usuário deletado com sucesso!'})
+});
 
 server.listen(3000);
 console.log("Server rodando na porta 3000");
